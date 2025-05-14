1101af133a0ef73e9840f730997814ce9ac2315d
Rohit Iyengar <rohitiyengar8@gmail.com>
2025-05-15 01:54:16 +0530

    Fixed PORTs

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const OpenAI = require('openai');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT;
 
 // Middleware
 app.use(cors());
